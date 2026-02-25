de37680c922d3cfcbf1393f6ded5b2b87b67f727
ayhido <menduse9@gmail.com>
2026-02-25 14:44:23 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,13 @@ import axios from "axios";
 
 const app = express();
 app.use(express.json());
+import path from "path";
+import { fileURLToPath } from "url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "public")));
 // ===============================
 // 🔥 Telegram настройки
 // ===============================
